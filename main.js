ce43086d2bc588bca54f1ad13fe6fa6eb78d3268
Jui-Young Chen <jyc422@gmail.com>
2016-04-18 01:54:21 -0700

    lots of portfolio changes

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
   $('.parallax').parallax();
   $('.scrollspy').scrollSpy();
+  $('.modal-trigger').leanModal();
 });
 
 $('nav a').click(function () {
+ var $href = $(this).attr('href');
+ $('body').stop().animate({
+   scrollTop: $($href).offset().top
+ }, 500);
+ return false;
+});
+
+$('#down').click(function () {
  var $href = $(this).attr('href');
  $('body').stop().animate({
    scrollTop: $($href).offset().top
